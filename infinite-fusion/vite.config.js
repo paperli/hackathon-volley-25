9c0a663acd6cddf7dfe99b8de86da1c44738b8c3
Paper <paper.li@gmail.com>
2025-07-09 08:20:53 -0700

    Settings to publish on github pages

--- infinite-fusion/vite.config.js
+++ infinite-fusion/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/infinite-fusion/', // the github repo name
   plugins: [react()],
   server: {
     allowedHosts: ['.ngrok-free.app'],
