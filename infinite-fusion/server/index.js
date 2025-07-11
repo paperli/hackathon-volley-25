26e42599b410a3c9cba12433f553b4f3117e3027
Paper <paper.li@gmail.com>
2025-07-11 15:29:17 -0700

    Remove dummy code

--- infinite-fusion/server/index.js
+++ infinite-fusion/server/index.js
@@ -6,32 +6,6 @@ const { OpenAI } = require('openai');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// CORS configuration: allowed origins
-// const allowedOrigins = [
-//   'http://localhost:5173',
-//   'https://paperli.github.io/infinite-fusion/',
-//   'https://paperworkstud.io/infinite-fusion/',
-// ];
-
-// if (process.env.FRONTEND_URL) {
-//   allowedOrigins.push(process.env.FRONTEND_URL);
-// }
-
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     // allow requests with no origin (like mobile apps or curl requests)
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   },
-//   methods: ['GET', 'POST', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true,
-// }));
-
 // CORS configuration: allow all origins
 app.use(cors());
 app.options('*', cors());
